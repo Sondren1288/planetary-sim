25483d1e6840f9a69297670d405733260ab5a73d
Sondren1288 <sondrenordmoen@hotmail.com>
2024-06-04 16:54:47 +0200

    and we continue

--- rootCompileTest/main.cpp
+++ rootCompileTest/main.cpp
@@ -1,6 +1,8 @@
 #include <TApplication.h>
 #include <TCanvas.h>
 #include <TButton.h>
+#include <TSystem.h>
+#include <TList.h>
 
 #include <iostream>
 
@@ -17,14 +19,23 @@ void someFunc() {
 }
 
 int main() {
-    TApplication *app = new TApplication("App", 0, 0);
+
+    TList *l = TApplication::GetApplications();
+    std::cout << "Is nullpointer?: " << (nullptr == l) << std::endl;
+    
+    if (l == nullptr) {
+        TApplication *app = new TApplication("App", 0, 0);
+    }
     TCanvas *canv = new TCanvas();
     canv->cd();
     TButton *but = new TButton("P1", "Main::someFunc()", 0.1, 0.1, 0.45, 0.9);
     TButton *but2 = new TButton("P2", "someFunc()", 0.55, 0.1, 0.9, 0.9);
     but->Draw();
     but2->Draw();
-    app->Run();
+    while (1) {
+        gSystem->ProcessEvents();
+    }
+    //app->Run();
     
 }
 
