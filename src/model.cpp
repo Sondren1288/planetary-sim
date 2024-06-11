57a5b008db3bb0f672abe9e46448f5745430577a
Sondren1288 <sondrenordmoen@hotmail.com>
2024-06-11 22:05:45 +0200

    More comments

--- src/model.cpp
+++ src/model.cpp
@@ -4,21 +4,37 @@
 #include "body.hpp"
 #include "model.hpp"
 
-
 using namespace model;
 
 
+
 Model::Model(double deltaT) {
+    /*
+     * Init function which takes a deltaT.
+     * deltaT represents the time passing between each plotted point
+     */
     this->deltaT = deltaT / 2;
     simTime = 0.0;
 }
 
+
+
 bool Model::addBody(body::Body newBody) {
+    /*
+     * Add a body to the model.
+     * Can be obtained with getBodyByName after
+     */
     bodies.push_back(newBody);
     return true;
 }
 
+
+
 body::Body Model::getBodyByName(std::string toFind) {
+    /*
+     * Return a body with the given name, if it exists.
+     * Else, returns an empty body
+     */
     for (body::Body current : bodies) {
         if (current.getName() == toFind) {
             return current;
@@ -27,7 +43,13 @@ body::Body Model::getBodyByName(std::string toFind) {
     return body::Body(0, {0, 0, 0});
 }
 
+
+
 double Model::iterate() {
+    /*
+     * Do one iteration of Velocity Verlet
+     * (Actually 2, since drawing speed is the main limiting factor)
+     */
     for (body::Body &body : bodies) {
         updateBody(body);
     }
@@ -42,21 +64,25 @@ double Model::iterate() {
     return simTime;
 }
 
+
+
 void Model::updateBody(body::Body &body) {
-    // Calculate the new position, and then do the the update?
-    // This means the new position has to be calculated before force
-    force totalForce = {0, 0, 0};
-    //position curPos = body.getPos();
-    //position newPos = body.step(deltaT);
-    
+    /*
+     * Do an update of given body with the Velocity Verlet method.
+     * First, updates position with the forces already acting upon the body.
+     * Then, finds the total force now acting upon the body.
+     * Then updates velocity and acceleration.
+     */
     body.updatePos(deltaT);
 
+    force totalForce = {0, 0, 0};
+    // Sums total forces acting on body
     for (body::Body &other : bodies) {
         if (body.getName() != other.getName()) {
             totalForce = plu(totalForce, body.getAttraction(other));
         }
     }
-    //body.update(deltaT, totalForce);
+    // Updates velocity given Velocity Verlet, then acceleration
     body.updateVerlet(deltaT, totalForce);
 }
 
